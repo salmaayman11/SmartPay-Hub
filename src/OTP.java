31fc70b08dacc8ea6db23f056b50c0050aa6303d
mariam0ashraf <mariam00ashraf@gmail.com>
2023-11-12 21:48:46 +0200

    OTP

---
+++ src/OTP.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OTP {
+    public static Boolean generateOTP(){
+        int random= (int) Math.random();
+        System.out.println(random);
+        System.out.println("Enter the num");
+        Scanner scanner=new Scanner(System.in);
+        int num= scanner.nextInt();
+        if(num==random){
+            return true;
+        }
+        return false;
+    }
+}
