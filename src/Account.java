0d30d8066f7213637f13f9b08d2deb3fdd13a064
mariam0ashraf <mariam00ashraf@gmail.com>
2023-11-13 00:29:45 +0200

    Account

--- src/Account.java
+++ src/Account.java
@@ -1,32 +1,46 @@
-abstract public class Account {
+import java.util.Scanner;
+
+public class Account {
     String username;
     String password;
-    utilityBills bills;
-    accountProvider provider;
+    AccountProvider provider;
 
-    public boolean OTP() {
-        System.out.println("main");
-        return true;
-    }
 
     public static Account signIn() {
         return null;
     }
 
     public void signUp() {
+        System.out.println("1. Bank account\n2. Wallet Account\nEnter option number");
+        Scanner scanner = new Scanner(System.in);
+        String opt = scanner.nextLine();
+        System.out.println("Enter mobile number");
+        String mobileNum = new Scanner(System.in).nextLine();
+
+        switch (opt) {
+            case "1":
+                setProvider(new walletProvider(mobileNum));
+                getProvider().verify();
+                break;
+            case "2":
+                System.out.println("Enter your account number");
+                String bankNum = new Scanner(System.in).nextLine();
+                setProvider(new bankProvider (bankNum,mobileNum);
+                getProvider().verify() ;
+
+        }
+        OTP otp=new OTP();
+        if(otp.generateOTP()){
+
+        }
     }
 
-    public boolean transferToACC(String username) {
-    }
-
-    public boolean transferToWallet(String mobile) {
-    }
-    public float inquire (){
-
-    }
-    public boolean payBill(){
-
+    public void setProvider(AccountProvider provider) {
+        this.provider = provider;
     }
 
 
+    public AccountProvider getProvider(){
+        return provider;
+    }
 }
--- src/InstaPay.java
+++ src/InstaPay.java
@@ -5,25 +5,9 @@ public class InstaPay {
     Transfer transferStrategy;
     Account account;
     public void signUp() {
-        System.out.println("1. Bank account\n2. Wallet Account\nEnter option number");
-        Scanner scanner = new Scanner(System.in);
-        String opt = scanner.nextLine();
-        System.out.println("Enter mobile number");
-        String mobileNum = new Scanner(System.in).nextLine();
 
-        switch (opt) {
-            case "1":
-                account = new Account();
-                account.setProvider(new walletProvider(mobileNum));
-                account.getProvider().verify();
-                break;
-            case "2":
-                System.out.println("Enter your account number");
-                String bankNum = new Scanner(System.in).nextLine();
-                account=new Account();
-                account.setProvider(new bankProvider (bankNum,mobileNum);
-                account.getProvider().verify() ;
-        }
+
+
     }
     public Boolean transfer( String type){
         return null;
