331cace8f041e8175ea1ea0e41c5860bf362be34
mariam0ashraf <mariam00ashraf@gmail.com>
2023-11-13 18:45:57 +0200

    transfer()-signin()-check()- verify()deleted-OTP

--- src/InstaPay.java
+++ src/InstaPay.java
@@ -2,15 +2,38 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class InstaPay {
-    UtilityBills bill;
+    UtilityBill bill;
     Transfer transferStrategy;
     Account account;
     public void signUp() {
         account.signUp();
     }
-    public Boolean transfer( String type){
-        if(type=="");
-        return null;
+    public Boolean transfer(String type){
+        if(type.equalsIgnoreCase("bank")){
+            if (!(account.getProvider() instanceof BankProvider)) {
+                System.out.println("You cannot transfer to bank account");
+                return false;
+            }
+            System.out.println("enter the account number you want to transfer to");
+            String accno=new Scanner((System.in)).nextLine();
+            System.out.println("Enter the amount you want to transfer");
+            float amount =new Scanner(System.in).nextFloat();
+
+            return transferStrategy.transfer(accno,amount);
+        }
+        else if(type.equalsIgnoreCase("instapay account")){
+            System.out.println("enter your username");
+            String user=new Scanner(System.in).nextLine();
+            float amount =new Scanner(System.in).nextFloat();
+            return transferStrategy.transfer(user,amount);
+        }
+        else if(type.equalsIgnoreCase("wallet")){
+            System.out.println("enter your Mobile number");
+            String mobile=new Scanner(System.in).nextLine();
+            float amount =new Scanner(System.in).nextFloat();
+            return transferStrategy.transfer(mobile,amount);
+        }
+        return false;
     }
     public String getMobile(){
         return account.provider.getMobile();
@@ -18,66 +41,40 @@ public class InstaPay {
     public float getBalance(){
         return account.provider.getBalance();
     }
-    public Boolean verify(){
-        return true;
-    }
-    public Boolean Bill(String Type){
-        System.out.println("1. Inquire - 2.Pay bill");
-        int option=new Scanner(System.in).nextInt();
-        double random= Math.random();
-        if(option==1) {
-            if(Objects.equals(Type, "Water")) {
-                System.out.println("Name: " +account.username);
-                System.out.println("Water bills"+random);
-                System.out.println("Do you want to pay bills y/n");
-                String choice = new Scanner(System.in).nextLine();
-                if (choice.equalsIgnoreCase("Y")) {
-                    return bill.pay(account.provider);
-
-            }
-                else
-                    return false;
-            }
-            if(Objects.equals(Type, "Electricity")) {
-                System.out.println("Name: " +account.username);
-                System.out.println("Electricity bills"+random);
-                System.out.println("Do you want to pay bills y/n");
-                String choice = new Scanner(System.in).nextLine();
-                if (choice.equalsIgnoreCase("Y")) {
-                    return bill.pay(account.provider);
-
+    public Boolean Bill(String Type) {
+        if (Type.equalsIgnoreCase("water") || Type.equalsIgnoreCase("electricity") || Type.equalsIgnoreCase("gas")) {
+            System.out.println("1. Inquire - 2.Pay bill");
+            int option = new Scanner(System.in).nextInt();
+            if (option == 1) {
+                if (Type.equalsIgnoreCase( "Water")) {
+                    bill = new Water();
                 }
-                else
-                    return false;
-            }
-            if(Objects.equals(Type, "Gas")) {
-                System.out.println("Name: " +account.username);
-                System.out.println("Gas bills"+random);
+                if (Type.equalsIgnoreCase( "Electricity")) {
+                    bill = new Electricity();
+                }
+                if (Type.equalsIgnoreCase( "Gas")) {
+                    bill = new Gas();
+                }
+                System.out.println("Name: " + account.username);
+                System.out.println("the bill of " + bill.getType() + bill.getAmount());
                 System.out.println("Do you want to pay bills y/n");
                 String choice = new Scanner(System.in).nextLine();
                 if (choice.equalsIgnoreCase("Y")) {
                     return bill.pay(account.provider);
 
-                }
-                else
+                } else
                     return false;
             }
 
-        }
-        if(option==2) {
-            if(Type=="Water"){
+            if (option == 2) {
                 return bill.pay(account.provider);
             }
-            if(Type=="Electricity"){
-                return bill.pay(account.provider);
-            }
-            if(Type=="Gas"){
-                return bill.pay(account.provider);
-            }
-
+            System.out.println("You entered wrong option");
+            return false;
         }
         return false;
     }
+
 
     public void signIn(String username,String pass){
         Account.signIn();
